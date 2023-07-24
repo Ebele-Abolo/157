acea2ef37462c2a3da090d9719680987c8859c49
Ebele-Abolo <77888430+Ebele-Abolo@users.noreply.github.com>
2023-07-23 22:15:15 -0500

    Add files via upload

---
+++ 157.py
@@ -0,0 +1,7 @@
+pokemon_list =[pikachu]
+power_list = [200]
+label["image"]=random_pokemon
+player1_score = player1_score  + random_power_list
+player2_score = player2_score +random_power_list2
+player_1_btn = Button(root, image = img, command = player1)
+player_2_btn = Button(root, image = img, command = player2)
